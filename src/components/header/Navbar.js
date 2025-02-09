0e375a2ffecef7cc21069a149ae170875cbbcc99
praba234 <kprabakaran947@gmail.com>
2025-02-09 23:56:16 +0530

    updated about

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -12,10 +12,9 @@ const Navbar = () => {
     {/* Navigation items on the right */}
     <div className="nav-items">
       <NavLink to="/" className="nav-item">Home</NavLink>
-      <NavLink to="/about" className="nav-item">Home</NavLink>
       <NavLink to="/project" className="nav-item">Project</NavLink>
       <NavLink to="/blog" className="nav-item">Blog</NavLink>
-
+      <NavLink to="/about" className="nav-item">About</NavLink>
     </div>
   </nav>
   );
