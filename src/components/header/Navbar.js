85670317a9d24996c8d2538ebf6a4d0114c05f8f
Poovarasan-P <poovarasanpalani7@gmail.com>
2025-02-11 23:08:45 +0530

    home page updated to mob responsive

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,25 +1,29 @@
-import { NavLink } from "react-router-dom";
-import { motion } from 'framer-motion';
-import "../Styles.css";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Ensure this is linked
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-    {/* Logo on the left */}
-    <div className="logo">｛ HARI ｝</div>
+      {/* Logo */}
+      <div className="logo">｛ HARI ｝</div>
 
-    {/* Navigation items on the right */}
-    <div className="nav-items">
-      <NavLink to="/" className="nav-item">Home</NavLink>
-      <NavLink to="/about" className="nav-item">About</NavLink>
-      <NavLink to="/project" className="nav-item">Projects</NavLink>
-      <NavLink to="/blog" className="nav-item">Blog</NavLink>
-      
-     
 
-    </div>
-  </nav>
+      {/* Hamburger Menu */}
+      <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      {/* Navigation Links */}
+      <ul className={`nav-items ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" className="nav-item" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/project" className="nav-item" onClick={() => setMenuOpen(false)}>Project</Link></li>
+        <li><Link to="/blog" className="nav-item" onClick={() => setMenuOpen(false)}>Blog</Link></li>
+        <li><Link to="/about" className="nav-item" onClick={() => setMenuOpen(false)}>About</Link></li>
+      </ul>
+    </nav>
   );
 };
 
